5c3815383183223305aaa4158c10d9219e3a5a25
Camotoy <20743703+Camotoy@users.noreply.github.com>
2021-03-04 20:27:03 -0500

    Jenkinsfile attempt 5 and add script to retrieve vanilla files

--- copy_java_files_to_pack.sh
+++ copy_java_files_to_pack.sh
@@ -1,9 +1,12 @@
 wget https://launcher.mojang.com/v1/objects/37fd3c903861eeff3bc24b71eed48f828b5269c8/client.jar
 unzip client.jar -d extracted/
-while read p; do
-  readarray -d , -t filesToCopy<<< "$p"
-  mkdir -p filesToCopy[1]
-  cp filesToCopy[0] filesToCopy[1]
+IFS=' '
+while read -r p; do
+  read -rafilesToCopy<<< "$p"
+  echo "extracted/${filesToCopy[0]} ${filesToCopy[1]}"
+  mkdir -p "${filesToCopy[1]}"
+  cp "extracted/${filesToCopy[0]}" "${filesToCopy[1]}"
 done <required_files.txt
+# Make sure the above file ends with a newline!!
 rm client.jar
 rm -r extracted
